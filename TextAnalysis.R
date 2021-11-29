095e9ea15edb2a43891bdea4b6e29f0c8af083a2
jcarbaugh7 <carbaugh@bu.edu>
2021-11-29 11:52:07 -0500

    Sentiment Analysis Start

--- TextAnalysis.R
+++ TextAnalysis.R
@@ -29,7 +29,69 @@ tidy_dq %>%
 #Sentiment Analysis Start
 nrc_joy <- get_sentiments("nrc") %>% 
   filter(sentiment == "joy")
-tidy_books %>%
-  filter(book == "Emma") %>%
+tidy_dq %>%
   inner_join(nrc_joy) %>%
   count(word, sort = TRUE)
+
+dq_sentiment <- tidy_dq %>%
+  inner_join(get_sentiments("bing")) %>%
+  count(index = chapter, sentiment) %>%
+  pivot_wider(names_from = sentiment, values_from = n, values_fill = 0) %>% 
+  mutate(sentiment = positive - negative)
+
+ggplot(dq_sentiment, aes(index, sentiment)) +
+  geom_col(show.legend = FALSE)
+
+
+afinn <- tidy_dq %>% 
+  inner_join(get_sentiments("afinn")) %>% 
+  group_by(index = linenumber %/% 200) %>% 
+  summarise(sentiment = sum(value)) %>% 
+  mutate(method = "AFINN")
+
+bing_and_nrc <- bind_rows(
+  tidy_dq %>% 
+    inner_join(get_sentiments("bing")) %>%
+    mutate(method = "Bing et al."),
+  tidy_dq %>% 
+    inner_join(get_sentiments("nrc") %>% 
+                 filter(sentiment %in% c("positive", 
+                                         "negative"))
+    ) %>%
+    mutate(method = "NRC")) %>%
+  count(method, index = linenumber %/% 200, sentiment) %>%
+  pivot_wider(names_from = sentiment,
+              values_from = n,
+              values_fill = 0) %>% 
+  mutate(sentiment = positive - negative)
+
+bind_rows(afinn, 
+          bing_and_nrc) %>%
+  ggplot(aes(index, sentiment, fill = method)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~method, ncol = 1, scales = "free_y")
+
+
+
+bing_word_counts <- tidy_dq %>%
+  inner_join(get_sentiments("bing")) %>%
+  count(word, sentiment, sort = TRUE) %>%
+  ungroup()
+bing_word_counts %>%
+  group_by(sentiment) %>%
+  slice_max(n, n = 10) %>% 
+  ungroup() %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(n, word, fill = sentiment)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~sentiment, scales = "free_y") +
+  labs(x = "Contribution to sentiment",
+       y = NULL)
+
+
+library(wordcloud)
+
+tidy_dq %>%
+  anti_join(stop_words) %>%
+  count(word) %>%
+  with(wordcloud(word, n, max.words = 100))
